617c513dcc9003d6d0ac42586ad07b9f6cce26a8
gaopengfei <1362955042@qq.com>
2015-06-17 17:11:21 +0800

    透明度的多物体运动框架

--- JS_Animation/js/opacity.js
+++ JS_Animation/js/opacity.js
@@ -3,15 +3,30 @@
  */
 
 window.onload = function () {
-    var oDiv = document.getElementById('div1');
+
+   /* var oDiv = document.getElementById('div1');
     oDiv.onmouseover = function () {
         startMove(100);
     }
     oDiv.onmouseout = function () {
         startMove(30);
+    }*/
+
+    var oDivs = document.getElementsByTagName('div');
+    for(var i = 0;i < oDivs.length;i++){
+        oDivs[i].timer = null;
+        oDivs[i].alpha = 30;
+        oDivs[i].onmouseover = function () {
+            startMove(this,100);
+        }
+        oDivs[i].onmouseout = function () {
+            startMove(this,30);
+        }
     }
+
 }
 
+/*
 var timer = null;
 var alpha = 30; // 引入一个变量
 function startMove(iTarget){
@@ -25,6 +40,20 @@ function startMove(iTarget){
             alpha += speed;
             oDiv.style.filter = 'alpha('+alpha+')'; // IE
             oDiv.style.opacity = alpha / 100;
+        }
+    },30);
+}*/
+
+function startMove(obj,iTarget){
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        var speed = obj.alpha > iTarget?-10:10;
+        if(obj.alpha == iTarget){
+            clearInterval(obj.timer);
+        }else{
+            obj.alpha += speed;
+            obj.style.filter = 'alpha('+ obj.alpha+')'; // IE
+            obj.style.opacity = obj.alpha / 100;
         }
     },30);
 }
