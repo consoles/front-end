bfc79418df0e98a7c97fff877cdba33cee116b01
gaopengfei <1362955042@qq.com>
2015-06-25 15:36:15 +0800

    使用js实现京东左侧导航——增加图片焦点显示

--- Category_Navigation/js/script.js
+++ Category_Navigation/js/script.js
@@ -7,19 +7,8 @@ window.onload = function () {
     var Lis = document.getElementsByTagName('li');
 
     for(var i = 0;i < Lis.length;i++){
-        Lis[i].i = i;
         Lis[i].onmouseover = function () {
             this.className = 'lihover';
-            var h0 = (this.i - 1) * 30 + 42;
-            var y = this.getElementsByTagName('div')[0].offsetHeight;
-            var h = this.getElementsByTagName('div')[0].style.top + y;
-
-            if(h < h0){
-                this.getElementsByName('div')[0].style.top = h0 + 'px';
-            }
-            if(y > 550){
-                his.getElementsByName('div')[0].style.top = 3 + 'px';
-            }
         }
         Lis[i].onmouseout = function () {
             this.className = '';
