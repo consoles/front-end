76d53a807b775507be9b61e4bf25223d4af8324b
gaopengfei <1362955042@qq.com>
2015-12-16 22:13:50 +0800

    The Jade Template，use jade in Browser and html2jade，add readme

---
+++ Jade/readme.md
@@ -0,0 +1,9 @@
+## Jade模板引擎
+
+- demo1: Jade基础使用，id，标签，类，嵌套标签，属性，如何处理转义，注释，内联脚本和样式
+- demo2：变量的声明和使用，遍历对象和数组，分支语句
+- demo3：mixin语法。jade最吸引人的地方，可以声明之后重用，支持嵌套
+- demo4：模板继承，更好的复用
+- demo5：jade通过插件的方式对markdown、less和coffee-script的支持
+- demo6：在浏览器通过`runtime.js`使用jade
+- demo7：使用`html2jade`将html转化为jade
--- Jade/runtime.js
+++ Jade/runtime.js
@@ -1,61 +1,12 @@
 function template(locals) {
-    var jade_debug = [new jade.DebugItem(1, "runtime.jade")];
-    try {
-        var buf = [];
-        var jade_mixins = {};
-        var jade_interp;
-
-        jade_debug.unshift(new jade.DebugItem(0, "runtime.jade"));
-        jade_debug.unshift(new jade.DebugItem(1, "runtime.jade"));
-        buf.push("<!--");
-        jade_debug.unshift(new jade.DebugItem(1, jade_debug[0].filename));
-        jade_debug.unshift(new jade.DebugItem(3, jade_debug[0].filename));
-        buf.push("Created by gaopengfei on 2015/12/15.");
-        jade_debug.shift();
-        buf.push("\n");
-        jade_debug.unshift(new jade.DebugItem(3, jade_debug[0].filename));
-        buf.push("");
-        jade_debug.shift();
-        jade_debug.shift();
-        buf.push("-->");
-        jade_debug.shift();
-        jade_debug.unshift(new jade.DebugItem(4, "runtime.jade"));
-        buf.push("<div>");
-        jade_debug.unshift(new jade.DebugItem(undefined, jade_debug[0].filename));
-        jade_debug.unshift(new jade.DebugItem(5, "runtime.jade"));
-        buf.push("<h3>");
-        jade_debug.unshift(new jade.DebugItem(undefined, jade_debug[0].filename));
-        jade_debug.unshift(new jade.DebugItem(5, jade_debug[0].filename));
-        buf.push("jade runtime call");
-        jade_debug.shift();
-        jade_debug.shift();
-        buf.push("</h3>");
-        jade_debug.shift();
-        jade_debug.unshift(new jade.DebugItem(6, "runtime.jade"));
-        buf.push("<p>");
-        jade_debug.unshift(new jade.DebugItem(undefined, jade_debug[0].filename));
-        jade_debug.unshift(new jade.DebugItem(6, jade_debug[0].filename));
-        buf.push("this is from jade pre-compile");
-        jade_debug.shift();
-        jade_debug.shift();
-        buf.push("</p>");
-        jade_debug.shift();
-        jade_debug.unshift(new jade.DebugItem(7, "runtime.jade"));
-        buf.push("<p>");
-        jade_debug.unshift(new jade.DebugItem(undefined, jade_debug[0].filename));
-        jade_debug.unshift(new jade.DebugItem(7, jade_debug[0].filename));
-        buf.push("we use the `jade --client runtime.jade` to create a file named `runtime.js`");
-        jade_debug.shift();
-        jade_debug.shift();
-        buf.push("</p>");
-        jade_debug.shift();
-        jade_debug.shift();
-        buf.push("</div>");
-        jade_debug.shift();
-        jade_debug.shift();
-        ;
-        return buf.join("");
-    } catch (err) {
-        jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "//\r\n   Created by gaopengfei on 2015/12/15.\r\n\r\ndiv\r\n    h3 jade runtime call\r\n    p this is from jade pre-compile\r\n    p we use the `jade --client runtime.jade` to create a file named `runtime.js`\r\n");
-    }
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    ;
+    var locals_for_with = (locals || {});
+    (function (isRuntime) {
+        buf.push("<!-- 在客户端使用jade，jade可以下载到浏览器中可以被单独使用--><!-- 但是仅仅支持比较先进的浏览器。--><!-- 推荐的做法是将jade模板预编译成js脚本，然后使用jade的runtime.js在浏览器中调用Created by gaopengfei on 2015/12/15.\n--><div><h3>jade runtime call " + (jade.escape((jade_interp = isRuntime) == null ? '' : jade_interp)) + "</h3><p>this is from jade pre-compile</p><p>we use the `jade --client --no-debug runtime.jade` to create a file named `runtime.js`</p><p>we can use `runtime.js` in other jade files</p></div>");
+    }.call(this, "isRuntime" in locals_for_with ? locals_for_with.isRuntime : typeof isRuntime !== "undefined" ? isRuntime : undefined));
+    ;
+    return buf.join("");
 }
